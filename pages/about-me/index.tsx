706b4c46c67622e8b7fae58f0747c6cdaf1c6984
Emeka <101795048+mekuzd@users.noreply.github.com>
2025-01-06 17:03:59 +0100

    import navbar to experience page

--- pages/about-me/index.tsx
+++ pages/about-me/index.tsx
@@ -1,9 +1,11 @@
 import ExperienceAnSkill from '@/components/experienceAndSkills'
 import SocialsAndcontactUs from '@/components/socialsAndContactUs'
+import Navbar from '@/components/utils/navbar'
 
 export default function AboutMe() {
   return (
     <>
+      <Navbar />
       <ExperienceAnSkill />
       <SocialsAndcontactUs />
     </>
